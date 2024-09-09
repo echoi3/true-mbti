73f245cdf810937c0d08ac43546724c00e24fe1d
Eric Choi <echoi33@Erics-MBP.attlocal.net>
2024-09-09 16:48:44 -0400

    minor fix

--- src/components/MbtiTest.js
+++ src/components/MbtiTest.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { collection, query, where, getDocs, updateDoc } from 'firebase/firestore';
+import { collection, query, where, getDocs, updateDoc, doc } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 
 function MbtiTest() {
